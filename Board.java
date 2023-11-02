cd6776f892ad988f6625ae2e424111696a77933f
jakewarrenblack <jakewarrenblack01@gmail.com>
2023-11-02 19:12:47 +0000

    Add javadoc explainer, comments and tidy slightly in Board

--- Board.java
+++ Board.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 
 public class Board {
-    //    Quadrant[] quadrants = new Quadrant[4];
     Point[] points = new Point[24];
     // We also need to have spaces for the bars and the off sections
     String[][] boardPrint = new String[15][14];
@@ -10,30 +9,49 @@ public class Board {
         for (int i = 0; i < 24; i++) {
             this.points[i] = new Point(i);
         }
+
+        // The top of the board
         boardPrint[0] = new String[]{"-13", "-+-", "-+-", "-+-", "-+-", "18-", "BAR", "-19", "-+-", "-+-", "-+-", "-+-", "-24", " OFF"};
+
+        // The bottom of the board
         boardPrint[14] = new String[]{"-12", "-+-", "-+-", "-+-", "-+-", "-7-", "BAR", "-6-", "-+-", "-+-", "-+-", "-+-", "-1-", " OFF"};
+
+        // The middle row, separating the two halves of the board
         boardPrint[7] = new String[]{"   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "    "};
 
     }
 
+    /**
+     * <b>Draws the board</b>
+     * <ul>
+     *     <li>Iterates over the quadrants (top-left, top-right, bottom-left, bottom-right)</li>
+     *     <li>For each quadrant, it calculates the starting row, column, and increment values</li>
+     *     <li>The boardPrint array represents the positions on the board, initialised to empty spaces</li>
+     *     <li>For each point in a quadrant, checks if there's a piece (using getColour)</li>
+     *     <li>If no piece, leave it blank. Otherwise, use the B/W value.</li>
+     * </ul>
+     */
     private void updateBoard() {
-        int point;
+
+        // Two outer loops iterate through 1. vertical and 2. horizontal quadrants.
         for (int vert = 0; vert < 2; vert++) {
             for (int hor = 0; hor < 2; hor++) {
-                int quadrant = hor + vert * 2;
+
                 int startRow = 14 * (1 - vert);
                 int increment = vert * 2 - 1;
                 int startCol = 13 - 7 * hor;
                 int col = startCol;
+
                 // Need to figure out how we're tracking the bar and off positions
                 for (int j = 0; j < 6; j++) {
                     boardPrint[startRow + (j + 1) * increment][col] = "   ";
                 }
+
                 for (int i = 0; i < 6; i++) {
-                    point = quadrant * 6 + i;
                     col = startCol - (i+1);
                     System.out.println("Row "+startRow+" Col "+col);
                     for (int j = 0; this.points[i].numPieces() > j; j++) {
+                        // Value will be blank, or a piece colour (B, W), if there's a piece here
                         boardPrint[startRow + (j + 1) * increment][col] = this.points[i].getColour();
                     }
                     for (int j = this.points[i].numPieces(); j < 6; j++) {
@@ -46,9 +64,9 @@ public class Board {
 
     public void print () {
         updateBoard();
-        for (int i = 0; i < boardPrint.length; i++) {
-            for (int j = 0; j < boardPrint[i].length; j++) {
-                System.out.print(boardPrint[i][j]);
+        for (String[] strings : boardPrint) {
+            for (String string : strings) {
+                System.out.print(string);
             }
             System.out.println();
         }
