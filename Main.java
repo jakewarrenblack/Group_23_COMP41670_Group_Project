fe8c41e3ec2cfa9e9b204f6b56c9021506f762f2
Jake <jakewarrenblack01@gmail.com>
2023-11-22 19:13:01 +0000

    Print out potential moves

--- Main.java
+++ Main.java
@@ -2,6 +2,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+    /**
+     *
+     * This is for figuring out where the player's piece would end up based on which dice value they use.
+     * White moves counter-clockwise (subtract)
+     * Black moves clockwise (add)
+     */
+    public static int calculate(int a, int b, boolean subtract) {
+        return subtract ? a - b : a + b;
+    }
+
     public static void main(String[] args) {
         Die die = new Die();
         Scanner in = new Scanner(System.in);
@@ -54,16 +65,10 @@ public class Main {
                 // The direction they can move depends on which player we're dealing with.
                 Point[] allBoardPoints = myGame.getBoard().getPoints();
 
-                int[] distanceOptions = {die.getCurrentValues()[0], die.getCurrentValues()[1], (die.getCurrentValues()[0] + die.getCurrentValues()[1])};
-
-                for(int option: distanceOptions){
-
-                }
-
+                int[] diceOptions = {die.getCurrentValues()[0], die.getCurrentValues()[1], (die.getCurrentValues()[0] + die.getCurrentValues()[1])};
 
                 ArrayList<Point> occupiedPoints = new ArrayList<>();
 
-                // FIXME: Actually, we DON'T need to iterate over every point, we need to iterate over every PIECE. The former would be pointlessly inefficient.
                 for(Point p: allBoardPoints){
                     System.out.println(p);
                     if(p.numPieces() != 0){
@@ -77,16 +82,50 @@ public class Main {
                 ArrayList<Piece> movablePieces = new ArrayList<>();
 
                 for(Point p: occupiedPoints){
-                    // FIXME: Well, maybe not fix, but I'm not 100% sure if the checker on 'top' of the pile is index 0 or index length-1?
-                    // FIXME: Also, it'd probably? be better if the Pieces got their position as their position within their Point,
-                    //  not just the position of the Point itself
                     Piece topPiece = p.getTopChecker();
 
                     if(topPiece.getColor() == currentPlayer.getColor()){
                      movablePieces.add(topPiece);
                     }
-
                 }
+
+                for(Piece p: movablePieces){
+                    //System.out.println("Movable piece " + p.getPosition());
+
+                    // black player will add dice values to determine move
+                    // white player will subtract dice values to determine move
+                    for(int option: diceOptions){
+                        System.out.println(
+                                "Potential move: " + p.getPosition() + "-" +
+                                calculate(p.getPosition(), option, currentPlayer.getColor() == Player.Color.WHITE)
+                        );
+
+                    }
+                }
+
+                // example scenario, jake (black player) goes first, options:
+                // 0, 11, 16, 18
+
+                // example, rolled a 5-3
+
+                // black moves counter clockwise
+
+                // options would be:
+                // use the 5:
+                // move 0-5
+                // move 11-16
+                // move 16-21
+                // move 18-23
+
+                // use the 3:
+                // move 0-3
+                // 11-14
+                // 16-19
+                // 18-21
+
+
+
+
 
                 // allBoardPoints is like this:
                 // 0:
@@ -98,19 +137,6 @@ public class Main {
                 //  black player rolls a 5-3
                 //  their options are to move one piece 5 places, and another piece 3 places, OR they can move one piece 8 places
                 // to give the player their move options, i need to iterate through all the points on the board, finding the relevant (black) pieces
-                // the pieces which can be moved are the ones at the top of the pile
-
-
-
-//                switch(currentPlayer.getColor()){
-//                    case BLACK: {
-//                        break;
-//                    }
-//                    case WHITE: {
-//                        break;
-//                    }
-//                }
-
 
 
                 if(move.equals("print")){
